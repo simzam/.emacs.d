3be135ad4bd51d783ac7f7afa283fa39204b9b1d
Simon Iversen <simon.iversen@protonmail.com>
2022-02-22 20:32:02 +0100

    Add js-doc.

--- lisp/web_dev.el
+++ lisp/web_dev.el
@@ -46,6 +46,15 @@
                                (add-hook 'xref-backend-functions #'xref-js2-xref-backend nil t)))
 )
 
+(use-package js-doc
+  :ensure t
+  :config
+  (add-hook 'js2-mode-hook
+          #'(lambda ()
+              (define-key js2-mode-map "\C-ci" 'js-doc-insert-function-doc)
+              (define-key js2-mode-map "@" 'js-doc-insert-tag)))
+  )
+
 ;; (use-package lsp-mode
 ;;   :ensure t
 ;;   :hook ((js-mode         ; ts-ls (tsserver wrapper)
