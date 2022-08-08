ba7120220789d94e5ad37da3cfc602042a1a25cc
Simon Iversen <simon.iversen@protonmail.com>
2022-08-08 17:30:55 +0200

    update

--- init.el
+++ init.el
@@ -3,87 +3,94 @@
 ;; Keywords: config, emacs
 ;;; Commentary:
 
-;; ;; introduce boon, evil package
+;; ;; TODO introduce evil package LATER
 
 ;;; Code:
+;;  configures main functionality and loads external config files
 
-;; color theme (load early to avoid blinking emacs window)
+;; Add config files from separate folder into load-path
+(add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
+(add-to-list 'load-path (expand-file-name "personal" user-emacs-directory))
+
+(require 'package)
+(setq package-enable-at-startup nil)
+(setq package-archives ())
+(add-to-list 'package-archives '("gnu" . "https://elpa.gnu.org/packages/") t)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+
+(unless (bound-and-true-p package--initialized)
+  (setq package-enable-at-startup nil)          ; To prevent initializing twice
+  (package-initialize))
+
+;; set use-package-verbose to t for interpreted .emacs,
+;; and to nil for byte-compiled .emacs.elc.
+(eval-and-compile
+  (setq use-package-verbose (not (bound-and-true-p byte-compile-current-file))))
+
+;; Ensures that use-package is installed.
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package)
+  (eval-when-compile (require 'use-package)))
+
+(use-package auto-package-update
+  ;; Keeps the packages updated
+  :ensure t
+  :if (not (daemonp))
+  :custom
+  (auto-package-update-interval 7) ;; in days
+  (auto-package-update-prompt-before-update t)
+  (auto-package-update-delete-old-versions t)
+  (auto-package-update-hide-results t)
+  :config
+  (auto-package-update-maybe))
+
+;;(load-library "org_config")
+;;(load-library "python_config")
+;; (load-library "web_dev")
+;;(load-library "latex_config")
+;;(load-library "personal_information")
+;;(load-library "custom-set")
+
 (use-package gruber-darker-theme
+  ;; color theme
   :ensure t
   :config
   (load-theme 'gruber-darker t))
-
-;; each environment has separate config file in a folder called 'lisp'
-(add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
-(load-library "startup")
-(load-library "org_config")
-(load-library "python_config")
-;; (load-library "web_dev")
-(load-library "latex_config")
-
-;; load file containing personal information and custom set variables.
-(add-to-list 'load-path (expand-file-name "personal" user-emacs-directory))
-(load-library "personal_information")
-(load-library "custom-set")
-
-
-(defalias 'yes-or-no-p 'y-or-n-p)
 
 (setq-default inhibit-startup-screen t
       initial-scratch-message nil
       ring-bell-function 'ignore
       make-backup-files nil
-      indent-tabs-mode -1)
+      indent-tabs-mode -1
+      tab-width 4
+	  make-backup-files nil
+	  indent-tabs-mode nil
+	  show-trailing-whitespace t
+	  visible-bell nil
+      calendar-week-start-day 1)
+
+(defalias 'yes-or-no-p 'y-or-n-p)
+
+;; Tool bar waste space
 (tool-bar-mode -1)
 
-(setq-default tab-width 4
-	      make-backup-files nil
-	      indent-tabs-mode nil
-	      show-trailing-whitespace t
-	      visible-bell nil
-          calendar-week-start-day 1)
-
-(global-unset-key (kbd "C-z"))
-(global-set-key (kbd "<f5>") 'revert-buffer)
-(global-set-key (kbd "C-x C-c") 'delete-frame)
+;; Indent when pressing RET
 (global-set-key (kbd "RET") 'newline-and-indent)
-
+;; Trailing whitespace are never welcome
 (add-hook 'before-save-hook 'delete-trailing-whitespace)
 
+;; Wrap text if text moves outside frame width
 (global-visual-line-mode 1)
+
+;; Highlight current line in active windoes
 (global-hl-line-mode 1)
-(show-smartparens-global-mode 1)
-(global-flycheck-mode)
-(global-company-mode)
+
+;; Always show line number for all frames
 (global-linum-mode)
 
-(use-package equake
-  :ensure t
-  ;; some examples of optional settings follow:
-  :custom
-  ;; set width a bit less than full-screen (prevent 'overflow' on multi-monitor):
-  (equake-size-width 0.99)
-  ;; set distinct face for Equake: white foreground with dark blue background, and different font:
-  :custom-face
-  (equake-buffer-face
-   ((t (:inherit 'default :family "DejaVu Sans Mono" :background "#000022" :foreground "white"))))
-  :config
-  ;; prevent accidental frame closure:
-  (advice-add #'save-buffers-kill-terminal :before-while #'equake-kill-emacs-advice)
-  ;; binding to restore last Equake tab when viewing a non-Equake buffer
-  (global-set-key (kbd "C-M-^") #'equake-restore-last-etab)
-  (global-set-key (kbd "C-z") 'equake-invoke)
-  ;; set default shell
-  (setq equake-default-shell 'shell)
-  ;; set list of available shells
-  (setq equake-available-shells
-   '("shell"
-     "vterm"
-     "rash"
-     "eshell")))
-
-;; module for a nice collection of smart key bindings
 (use-package crux
+  ;; Provides clean shortcuts for common tasks
   :ensure t
   :bind
   (("C-a" . crux-move-beginning-of-line)
@@ -103,31 +110,22 @@
    ("C-<" . mc/mark-previous-like-this)
    ("C-c C-<" . mc/mark-all-like-this)))
 
-;; quickly marking/unmarking various structures of text
+;; TODO use in for quick region selection latex python
 (use-package expand-region
+  ;; quickly mark/unmark regions of text
   :ensure t
   :bind
-  (("C-=" . er/expand-region)
-  ("C-M-=" . er/contract-region)))
+  (("C-," . er/expand-region)
+   ("C-M-," . er/contract-region)))
 
-;; package for smart handling of delimiters ("(","[", etc.)
-(use-package smartparens
-  :ensure t
-  :config
-  (show-smartparens-global-mode 1)
-  ;; (add-hook 'prog-mode-hook 'smartparens-mode)
-  :bind
-  (("M-(" . sp-wrap-round)
-  ("M-[" . sp-wrap-square)
-  ("M-{" . sp-wrap-curly)))
-
-(use-package projectile
-  :ensure t
-  :init
-  (projectile-mode +1)
-  :bind (:map projectile-mode-map
-              ("s-p" . projectile-command-map)
-              ("C-c p" . projectile-command-map)))
+;; TODO: add package
+;; (use-package projectile
+;;   :ensure t
+;;   :init
+;;   (projectile-mode +1)
+;;   :bind (:map projectile-mode-map
+;;               ("s-p" . projectile-command-map)
+;;               ("C-c p" . projectile-command-map)))
 
 (use-package company
   :ensure t
@@ -145,7 +143,10 @@
   (setq company-idle-delay .2)
   (setq company-echo-delay 0))
 
+;; TODO fix keybindings and sp pair nil?
 (use-package smartparens
+  ;; package for smart handling and navigation of delimiters ("(","[", etc.)
+  :ensure t
   :init
   (bind-key "C-M-f" #'sp-forward-sexp smartparens-mode-map)
   (bind-key "C-M-b" #'sp-backward-sexp smartparens-mode-map)
@@ -159,10 +160,14 @@
 
   :config
   (smartparens-global-mode t)
-
   (sp-pair "'" nil :actions :rem)
   (sp-pair "`" nil :actions :rem)
-  (setq sp-highlight-pair-overlay nil))
+  (setq sp-highlight-pair-overlay nil)
+
+  :bind
+  (("M-(" . sp-wrap-round)
+  ("M-[" . sp-wrap-square)
+  ("M-{" . sp-wrap-curly)))
 
 (use-package pdf-tools
   :ensure t
@@ -174,34 +179,23 @@
   (add-hook 'pdf-tools-enabled-hook 'pdf-view-midnight-minor-mode)
   (setq-default pdf-view-display-size 'fit-page))
 
+
+;; TODO how does
 (use-package flycheck
   :ensure t
   :init
-  (global-flycheck-mode t)
-  :config
-  (remove-hook 'flymake-diagnostic-functions 'flymake-proc-legacy-flymake))
-
-;; (use-package ace-window
-;;   :ensure t
-;;   :config
-;;   (global-set-key (kbd "M-p") 'ace-select-window))
+  (global-flycheck-mode t))
+  ;; :config
+  ;; TODO check this
+  ;; (remove-hook 'flymake-diagnostic-functions 'flymake-proc-legacy-flymake))
 
 (use-package undo-tree
   :ensure t
   :init
   (global-undo-tree-mode 1)
-  (global-set-key (kbd "C-q") 'undo)
-  (global-set-key (kbd "C-S-q") 'undo-tree-redo)
-  (undo-tree-mode 1)
   :config
   (with-eval-after-load 'undo-tree
-  (setq undo-tree-auto-save-history nil)))
-
-;; (use-package sml-mode
-;;   :ensure t
-;;   :init
-;;   (setenv "PATH" (concat "/usr/bin/sml" (getenv "PATH")))
-;;   (setq exec-path (cons "/usr/bin/sml"  exec-path)))
+    (setq undo-tree-auto-save-history nil)))
 
 (use-package magit
   :ensure t
@@ -212,9 +206,10 @@
    ("C-c z" . magit-log-buffer-file)
    ("C-c b" . magit-blame)))
 
-(use-package delight
-  :ensure t)
+;; TODO what does this do
+(use-package delight :ensure t)
 
+;; TODO configure helm
 (use-package helm
   :ensure t
   :delight
@@ -225,45 +220,23 @@
   :init
   (helm-mode t)
   :config
-  (use-package helm-flyspell :after (helm flyspell))
-  (use-package helm-xref)
-  (use-package helm-rg)
+  ;; (use-package helm-flyspell :after (helm flyspell))
+  (use-package helm-xref :ensure t)
+  (use-package helm-rg :ensure t)
   (helm-autoresize-mode 1)
-
   (setq helm-display-function 'helm-display-buffer-in-own-frame
-        helm-display-buffer-reuse-frame t
         helm-use-undecorated-frame-option t)
-
   (global-set-key (kbd "M-y") 'helm-show-kill-ring)
   (global-set-key (kbd "C-x b") 'helm-mini)
   (set-face-attribute 'helm-selection nil
                     :background "purple"
                     :foreground "white"))
 
-(use-package helm-spotify-plus
-  :ensure t
-  :config
-  (global-set-key (kbd "C-c s s") 'helm-spotify-plus)  ;; s for SEARCH
-  (global-set-key (kbd "C-c s f") 'helm-spotify-plus-next)
-  (global-set-key (kbd "C-c s b") 'helm-spotify-plus-previous)
-  (global-set-key (kbd "C-c s p") 'helm-spotify-plus-play)
-  (global-set-key (kbd "C-c s g") 'helm-spotify-plus-pause) ;; g cause you know.. C-g stop things :)
-)
-
 (use-package yasnippet
   :ensure t
   :init
   (yas-global-mode 1)
   (use-package yasnippet-snippets))
-
-(use-package flyspell
-  :commands flyspell-mode
-  :config
-  (setq ispell-program-name "Aspell"
-        ispell-extra-args '("--sug-mode=ultra"))
-  (add-hook 'text-mode-hook #'flyspell-mode)
-  (add-hook 'org-mode-hook #'flyspell-mode)
-  (add-hook 'prog-mode-hook #'flyspell-prog-mode))
 
 (use-package spaceline
   :ensure t
@@ -287,6 +260,54 @@
   (setq which-key-side-window-location 'right)
   (setq which-key-side-window-max-width 0.33))
 
+(use-package helm-spotify-plus
+  ;; Control Spotify from Emacs
+  :ensure t
+  :bind
+  (("C-c s s" . helm-spotify-plus)
+   ("C-c s f" . helm-spotify-plus-next)
+   ("C-c s b" . helm-spotify-plus-previous)
+   ("C-c s p" . helm-spotify-plus-play)
+   ("C-c s g" . helm-spotify-plus-pause)))
+
+;; (use-package equake
+;;   :ensure t
+;;   ;; some examples of optional settings follow:
+;;   :custom
+;;   ;; set width a bit less than full-screen (prevent 'overflow' on multi-monitor):
+;;   (equake-size-width 0.99)
+;;   ;; set distinct face for Equake: white foreground with dark blue background, and different font:
+;;   :custom-face
+;;   (equake-buffer-face
+;;    ((t (:inherit 'default :family "DejaVu Sans Mono" :background "#000022" :foreground "white"))))
+;;   :config
+;;   ;; prevent accidental frame closure:
+;;   (advice-add #'save-buffers-kill-terminal :before-while #'equake-kill-emacs-advice)
+;;   ;; binding to restore last Equake tab when viewing a non-Equake buffer
+;;   (global-set-key (kbd "C-M-^") #'equake-restore-last-etab)
+;;   (global-set-key (kbd "C-z") 'equake-invoke)
+;;   ;; set default shell
+;;   (setq equake-default-shell 'shell)
+;;   ;; set list of available shells
+;;   (setq equake-available-shells
+;;    '("shell"
+;;      "vterm"
+;;      "rash"
+;;      "eshell")))
+
+
 (provide 'init)
 ;;; init.el ends here
-(put 'downcase-region 'disabled nil)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(auto-package-update cmake-mode zenburn-theme yasnippet-snippets yasnippet-classic-snippets xref-js2 which-key web-mode virtualenvwrapper virtualenv use-package undo-tree sphinx-doc spaceline sml-mode smartparens slime rainbow-delimiters pythonic pytest py-autopep8 projectile pdf-tools panda-theme org-pomodoro org nov multiple-cursors magit lsp-mode jupyter js-doc iter2 iedit htmlize helm-xref helm-spotify-plus helm-rg helm-flyspell helm-bind-key haskell-mode gruber-darker-theme goto-last-change go-snippets flymake-jslint flymake-grammarly flymake-go flymake-eslint flymake-diagnostic-at-point flycheck-pyflakes expand-region esup ess eslint-fix equake elpy django-snippets diminish delight default-font-presets dash-functional crux company-ycmd company-jedi company-go company-box company-auctex benchmark-init auto-virtualenv auto-dictionary auto-complete auctex-latexmk ahungry-theme ace-window ac-js2)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
--- lisp/startup.el
+++ lisp/startup.el
@@ -17,27 +17,11 @@
   (package-refresh-contents)
   (package-install 'use-package))
 
-(use-package benchmark-init
+(use-package esup
   :ensure t
+  :pin melpa
   :init
-  ;; increase size of garbage collection during startup
-  (setq gc-cons-threshold 10000000)
+  (setq esup-depth 0))
 
-  ;; restore after startup
-  (add-hook 'after-init-hook
-            (lambda ()
-              (setq gc-cons-threshold 1000000)
-              (message "gc-cons-threshold restored to %S"
-                       gc-cons-threshold)))
-  :config
-  ;; to disable collection of benchmark data after init is done.
-  (add-hook 'after-init-hook 'benchmark-init/deactivate))
-
-  ;; restore after startup
-  (add-hook 'after-init-hook
-            (lambda ()
-              (setq gc-cons-threshold 1000000)
-              (message "gc-cons-threshold restored to %S"
-                       gc-cons-threshold)))
 (provide 'startup)
 ;;; startup ends here
